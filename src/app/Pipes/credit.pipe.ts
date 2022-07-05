84c0104358df3db06a15bceb0308c2dd0d2554e5
ehab0barakat <mrehab9797@gmail.com>
2022-07-05 05:19:13 +0200

    day02

---
+++ src/app/Pipes/credit.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'credit'
+})
+export class CreditPipe implements PipeTransform {
+
+  transform(value:string): string {
+    let one = value.substring(0,4);
+    let two = value.substring(4,8);
+    let three = value.substring(8,12);
+    let four = value.substring(12,16);
+
+    let finall = one +"-"+ two +"-"+ three +"-"+  four;
+
+    return finall;
+
+  }
+
+}
---
+++ src/app/Pipes/id-card.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { IdCardPipe } from './id-card.pipe';
+
+describe('IdCardPipe', () => {
+  it('create an instance', () => {
+    const pipe = new IdCardPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
